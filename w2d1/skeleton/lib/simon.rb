5f1c9e36007328ae619e90ce7af31419b838ba30
Adrian <adrianjewell91@gmail.com>
2017-08-05 20:51:22 -0700

    w2d2 simon playable game

--- w2d1/skeleton/lib/simon.rb
+++ w2d1/skeleton/lib/simon.rb
@@ -21,7 +21,7 @@ class Simon
   def take_turn
     show_sequence
     require_sequence
-    unless game_over
+    unless @game_over
       round_success_message
       @sequence_length += 1
     end
@@ -29,10 +29,14 @@ class Simon
 
   def show_sequence
     add_random_color
+    puts @seq
+    clear
+    puts "Please input the sequence (use a comma between words):"
   end
 
   def require_sequence
-
+    input = gets.chomp.split(",")
+    @game_over = true if input != @seq
   end
 
   def add_random_color
@@ -40,16 +44,29 @@ class Simon
   end
 
   def round_success_message
-
+    puts "Good work! Try the next round"
+    clear
   end
 
   def game_over_message
-
+    puts "Loser, try again next time"
+    clear
   end
 
   def reset_game
     @sequence_length = 1
     @game_over = false
-    @seq = [] 
+    @seq = []
   end
+
+  def clear
+    sleep 1
+    system "clear"
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  game = Simon.new
+
+  game.play
 end
