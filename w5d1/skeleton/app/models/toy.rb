a49c3c49f04c1ab0af29cd90a40805cc57339c51
Adrian <adrianjewell91@gmail.com>
2017-08-27 09:13:39 -0700

    polymorphic

--- w5d1/skeleton/app/models/toy.rb
+++ w5d1/skeleton/app/models/toy.rb
@@ -1,4 +1,6 @@
 class Toy < ApplicationRecord
-  validates :name, uniqueness: { scope: [:toyable] }
+  validates :name, uniqueness: { scope: [:toyable] } #I thought it could go the other way too.
   belongs_to :toyable, polymorphic: true
+
+
 end
